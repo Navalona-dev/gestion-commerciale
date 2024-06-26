d6dacf3b61ad5ba4c790bf59b075e1f8c4cb3e5e
lynah nomenjanahary <lynah.anjarasoa-nomenjanahary@start-upworld.org>
2024-06-26 12:10:06 +0300

    refactoring page login

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -313,25 +313,6 @@
 
 })();
 
-
-$(document).ready(function() {
-  $('#loginPassword-toggle').click(function() {
-    togglePasswordVisibility($('#login_form_password'));
-  });
-});
-
-function togglePasswordVisibility(passwordField) {
-  var fieldType = passwordField.attr('type');
-  
-  if (fieldType === 'password') {
-      passwordField.attr('type', 'text');
-      $('.btn-eye i').removeClass('bi-eye-slash').addClass('bi-eye');
-  } else {
-      passwordField.attr('type', 'password');
-      $('.btn-eye i').removeClass('bi-eye').addClass('bi-eye-slash');
-  }
-}
-
 $(document).ready(function() {
   $('#search-app').on('input', function() {
       var searchValue = $(this).val().toLowerCase();
