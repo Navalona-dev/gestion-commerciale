cee22b3ed8935b4fbeb005dffe19f19ead44feb9
lynah nomenjanahary <lynah.anjarasoa-nomenjanahary@start-upworld.org>
2024-07-03 21:29:01 +0300

    transfert produit

---
+++ src/Form/TransfertType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Application;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
+class TransfertType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('application', EntityType::class, [
+                'class' => Application::class,
+                'attr' => [
+                    'class' => 'form-control form-control-md chosen-select mb-3'
+                ],
+                'choice_label' => 'entreprise'
+            ])
+            ->add('qtt', NumberType::class, [
+                'attr' => [
+                    'class' => 'form-control form-control-md mb-3',
+                    'autocomplete' => 'off'
+                ],
+                'required' => true
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
