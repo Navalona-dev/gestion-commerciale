930bcec4942088dec19e046cbcf3697a4d26df4e
navalona <nnavalona@gmail.com>
2024-10-13 20:28:40 +0300

    fix suppression remise

--- src/Controller/Admin/AffaireController.php
+++ src/Controller/Admin/AffaireController.php
@@ -1125,7 +1125,7 @@ class AffaireController extends AbstractController
         $idProduit = $request->get('idProduit');
 
         $idAffaire = $request->get('idAffaire');
-
+        
         $type = $request->get('type');
 
         $isFrais = $request->get('isFrais');
@@ -1210,7 +1210,8 @@ class AffaireController extends AbstractController
             'montantPourcent' => $montantPourcent,
             'tvaVentes' => [],
             'option' => $option,
-            'isFrais' => $isFrais
+            'isFrais' => $isFrais,
+            'idAffaire' => $idAffaire
         ]);
     }
 
@@ -1375,7 +1376,6 @@ class AffaireController extends AbstractController
         }
 
         return $this->render("admin/affaires/reloadFinanciereProduct.html.twig", [
-            'applicationCurrentInSession' => $applicationCurrentInSession,
             'idApplication' => $this->application->getId(),
             'affaire' => $affaire,
             'produits' => $produits,
@@ -1393,4 +1393,93 @@ class AffaireController extends AbstractController
         //return $this->redirect($uri);
     }
 
+    /**
+     * @param Request $request
+     * @param ProduitRepository $produitRepository
+     * @param AffaireRepository $affaireRepository
+     * @Route("/financiere/deleteAddRemise/ajax", name="_delete_add_remise_product_ajax", methods={"POST"})
+     * @return Response
+     */
+    public function deleteAddRemise(Request $request, ProductRepository   $productRepository, AffaireRepository $affaireRepository, ProductService   $productService)
+    {
+        $idProduit = $request->get('idProduit');
+
+        $idAffaire = $request->get('idAffaire');
+
+        $type = $request->get('type');
+
+        $montantRemiseFinale = $request->get('montantRemiseFinale');
+
+        $uri = $request->get('uri');
+
+        $produit = [];
+
+        $affaire = [];
+        switch ($type) {
+            case "produit":
+                $produit = $productRepository->find($idProduit);
+
+                $affaires = $produit->getAffaires();
+
+                $affaire = $affaires[0];
+
+                $remiseAffaire = $affaire->getRemiseProduit();
+
+                if (null != $remiseAffaire) {
+                } else {
+                    $remiseAffaire = 0;
+                }
+
+                $affaire->setRemiseProduit($remiseAffaire - $produit->getRemise());
+
+                $produit->setRemise(null);
+
+                $produit->setRemisePourcent(null);
+
+                $productService->persist($produit);
+
+                $productService->persist($affaire);
+
+                break;
+
+            case "affaire":
+                $affaire = $affaireRepository->find($idAffaire);
+
+                $affaire->setRemise(null);
+
+                $affaire->setRemisePourcent(null);
+
+                $productService->persist($affaire);
+
+                break;
+        }
+
+        $productService->update();
+
+       // $montantCA = $produitService->updateCA($affaire, $produitRepository);
+
+       $produits = $this->productService->findProduitAffaire($affaire);
+
+       $facturesValide = [];
+       if ($affaire->getPaiement() != null && count($affaire->getFactures()) > 0) {
+           $factures = $affaire->getFactures();
+           $facturesValide = $factures->filter(function ($item) use ($affaire) {
+               return ($item->isValid() && 'regle' === $item->getStatut());
+               
+           });
+       }
+
+       return $this->render("admin/affaires/reloadFinanciereProduct.html.twig", [
+           'idApplication' => $this->application->getId(),
+           'affaire' => $affaire,
+           'produits' => $produits,
+           'montantCA' =>  null, // $montantCA - $montantRemiseFinale,
+           'affaireRemise' => $montantRemiseFinale,
+           'tvaVentes' => [],
+           'statuts' => $affaire::STATUT,
+           'application' => $this->application,
+           'factureFile' => (count($facturesValide) > 0 ? $facturesValide[count($facturesValide) - 1]->getFile(): null)
+
+       ]);
+    }
 }
